715fc2663043033d0955eda11ed2aeb427595e05
danny galimi <dannygalimi13@aim.com>
2022-03-27 12:08:39 -0400

    added validations to models

--- city-census-backend/app/models/city.rb
+++ city-census-backend/app/models/city.rb
@@ -1,3 +1,4 @@
 class City < ApplicationRecord
     has_many :relocations
+    validates :name, :population, presence: true
 end
--- city-census-backend/app/models/relocation.rb
+++ city-census-backend/app/models/relocation.rb
@@ -1,2 +1,5 @@
 class Relocation < ApplicationRecord
+    belongs_to :city
+    validates :amount, prescense: true
+    validates_inclusion_of :type, :in => ['arrival', 'departure']
 end
