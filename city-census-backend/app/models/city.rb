9fa94e5468d6d87ee9a1b9eeeb98363210366f27
danny galimi <dannygalimi13@aim.com>
2022-03-27 14:25:06 -0400

    created update_population method in the City model

--- city-census-backend/app/models/city.rb
+++ city-census-backend/app/models/city.rb
@@ -1,4 +1,18 @@
 class City < ApplicationRecord
     has_many :relocations
     validates :name, :population, presence: true
+
+    def update_population(relocation)
+        if relocation.kind == 'arrival'
+            self.population = self.population + relocation.amount
+            self.save
+        elsif relocation.kind == 'departure'
+            if self.population >= relocation.amount
+            self.population = self.population - relocation.amount
+            self.save
+            else
+                return 'Population cannot be less than 0'
+            end 
+        end 
+    end 
 end
--- city-census-backend/db/seeds.rb
+++ city-census-backend/db/seeds.rb
@@ -1,3 +1,5 @@
 city = City.create(name: 'New York City', population: 35000000)
 
 relocation = Relocation.create(city_id: 1, amount: 1000, kind: 'departure', date: Date.today, reason: 'Closed Business')
+
+relocation_two = Relocation.create(city_id: 1, amount: 1000, kind: 'arrival', date: Date.today, reason: 'Open Business')
