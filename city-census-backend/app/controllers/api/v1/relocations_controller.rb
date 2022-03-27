6dd46c1257d4eb19a90a802d57b6fdf0b67833af
danny galimi <dannygalimi13@aim.com>
2022-03-27 14:51:47 -0400

    added full CRUD actions to relocations controller

--- city-census-backend/app/controllers/api/v1/relocations_controller.rb
+++ city-census-backend/app/controllers/api/v1/relocations_controller.rb
@@ -1,25 +1,35 @@
 class Api::V1::RelocationsController < ApplicationController
 
-    def index 
-
-    end 
-
-    def create
-
-    end 
-
-    def show
-
-    end 
-
-    def destroy
-
-    end 
-
-    private
-
-    def relocation_params
-
-    end 
+        def index 
+            @relocations = Relocation.all 
+            render json: @relocations
+        end 
+    
+        def create
+            @relocation = Relocation.new(relocation_params)
+            if @relocation.save 
+                render json: @relocation 
+            else 
+                render json: {error: 'Error creating relocation'}
+            end 
+        end 
+    
+        def show
+            @relocation = Relocation.find(params[:id])
+            render json: @relocation
+        end 
+    
+        def destroy
+            @relocation = Relocation.find(params[:id])
+            @relocation.destroy
+        end 
+    
+        private
+    
+        def relocation_params(params)
+            params.require(:relocation).permit(:amount, :city_id, :kind, :date, :reason)
+        end 
+        
+    end
 
 end
