6214d70a097714062abc5f8275c948cb17d17b16
danny galimi <dannygalimi13@aim.com>
2022-03-27 14:31:36 -0400

    added index and params actions to cities controller

--- city-census-backend/app/controllers/api/v1/cities_controller.rb
+++ city-census-backend/app/controllers/api/v1/cities_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::CitiesController < ApplicationController
+
+        def index 
+            @citys = City.all 
+            render json: @cities
+        end 
+    
+        def create
+    
+        end 
+    
+        def show
+    
+        end 
+    
+        def destroy
+    
+        end 
+    
+        private
+    
+        def city_params(params)
+            params.require(:city).permit(:name, :population)
+        end 
+        
+    end
+    
 end
--- city-census-backend/app/controllers/api/v1/relocations_controller.rb
+++ city-census-backend/app/controllers/api/v1/relocations_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::RelocationsController < ApplicationController
+
+    def index 
+
+    end 
+
+    def create
+
+    end 
+
+    def show
+
+    end 
+
+    def destroy
+
+    end 
+
+    private
+
+    def relocation_params
+
+    end 
+
 end
