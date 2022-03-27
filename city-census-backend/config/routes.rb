f00d4a3e6a833e520bd456c9145a5ffebef4c2d4
danny galimi <dannygalimi13@aim.com>
2022-03-27 12:20:27 -0400

    namedspaced routes using api/v1

--- city-census-backend/config/routes.rb
+++ city-census-backend/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :relocations
-  resources :cities
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+
+    namespace :api do 
+      namespace :v1 do
+        resources :relocations
+        resources :cities
+      end 
+    end 
+  
+end   
