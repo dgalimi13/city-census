fddd9349f1615d5dc6b8b1756f1e93a318a06df0
danny galimi <dannygalimi13@aim.com>
2022-03-28 14:55:56 -0400

    store is accessible through react and redux dev tools, working properly

--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import cityReducer from './reducers/cityReducer'
+
 
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(cityReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
     <Provider store={store}>
         <App /> 
-    </Provider>
-    ,
+    </Provider>,
     document.getElementById('root'));
+
 
 
--- src/reducers/cityReducer.js
+++ src/reducers/cityReducer.js
@@ -1,8 +1,6 @@
 
-export default function cityReducer(state = {cities: []} ) {
+export default function cityReducer(state = {cities: []}, action) {
 
-
+return state
 
 }
-
-dispatchEvent(actionObje)
