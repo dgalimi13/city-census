12e09a05a90cb83a6cea8cf3497eee657225d99c
danny galimi <dannygalimi13@aim.com>
2022-03-28 15:36:21 -0400

    added fetch get request to fetchCities action creator

--- src/actions/fetchCities.js
+++ src/actions/fetchCities.js
@@ -2,9 +2,9 @@
 
 export function fetchCities() {
 
-    // fetch('http://127.0.0.1:3000/api/v1/cities')
-    // .then(resp => resp.json())
-    // .then(data => console.log(data))
+    fetch('http://127.0.0.1:3000/api/v1/cities')
+    .then(resp => resp.json())
+    .then(data => console.log(data))
 
 
 }
