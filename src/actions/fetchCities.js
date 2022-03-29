5f0eb8454bbe32099b241911c96aa79179e05461
danny galimi <dannygalimi13@aim.com>
2022-03-29 13:46:15 -0400

    added action keys to fetchCities function

--- src/actions/fetchCities.js
+++ src/actions/fetchCities.js
@@ -1,11 +1,13 @@
+import Cities from "../components/Cities"
 
 
 export function fetchCities() {
     return (dispatch) => {
-    console.log('inside fetch cities')
-    fetch('http://127.0.0.1:3000/api/v1/cities')
-    .then(resp => resp.json())
-    .then(data => console.log(data))
-
-     }
+        fetch('http://127.0.0.1:3000/api/v1/cities')
+        .then(resp => resp.json())
+        .then(cities => dispatch({
+            type: 'FETCH_CITIES',
+            payload: cities
+        }))
+    }
 }
