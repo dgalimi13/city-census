b05c7782db14bed4d9c5c9a25d9e7349da0a9d44
danny galimi <dannygalimi13@aim.com>
2022-03-29 14:44:32 -0400

    added initial local state for form, added values to inputs using this.state

--- src/components/CityInput.js
+++ src/components/CityInput.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 class CityInput extends React.Component {
 
-
+state = {name: '', population: ''}
 
     render () {
         return (
             <div>
                 <form>
                     <label>City Name: </label>
-                    <input type='text' placeholder='Name'/>
+                    <input type='text' placeholder='Name' value={this.state.name}/><br/>
                     <label>City Population: </label>
-                    <input type='text' placeholder='Population'/>
+                    <input type='text' placeholder='Population' value={this.state.population}/>
                 </form>
             </div>
 
