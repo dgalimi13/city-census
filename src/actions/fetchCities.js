ab4b432cdb580d123b262eb147c6639d9d9a7623
danny galimi <dannygalimi13@aim.com>
2022-03-29 13:36:02 -0400

    CitiesContainer has access to state using props returned as empty array

--- src/actions/fetchCities.js
+++ src/actions/fetchCities.js
@@ -1,6 +1,7 @@
 
 
 export function fetchCities() {
+    console.log('inside fetch cities')
     fetch('http://127.0.0.1:3000/api/v1/cities')
     .then(resp => resp.json())
     .then(data => console.log(data))
--- src/reducers/cityReducer.js
+++ src/reducers/cityReducer.js
@@ -1,7 +1,7 @@
 
 export default function cityReducer(state = {cities: []}, action) {
 
-    
+    return state
 
 
 
