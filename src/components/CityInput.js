e1bd11741eb87eb746da94ba6fe50f63eed5db83
danny galimi <dannygalimi13@aim.com>
2022-03-29 14:57:37 -0400

    created handleChange funtion and added it on onChange event handler for form name and population inputs

--- src/components/CityInput.js
+++ src/components/CityInput.js
@@ -4,14 +4,20 @@ class CityInput extends React.Component {
 
 state = {name: '', population: ''}
 
+handleChange = (event) => {
+    this.setState({
+        [event.target.name]: event.target.value
+    })
+}
+
     render () {
         return (
             <div>
                 <form>
                     <label>City Name: </label>
-                    <input type='text' placeholder='Name' value={this.state.name}/><br/>
+                    <input type='text' placeholder='Name' value={this.state.name} name="name" onChange={this.handleChange}/><br/>
                     <label>City Population: </label>
-                    <input type='text' placeholder='Population' value={this.state.population}/>
+                    <input type='text' placeholder='Population' value={this.state.population} name="population" onChange={this.handleChange}/>
                 </form>
             </div>
 
