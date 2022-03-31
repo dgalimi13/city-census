f56a465772f9da3e6626ba49528ac7f2b730c5dd
danny galimi <dannygalimi13@aim.com>
2022-03-31 11:22:43 -0400

    imported switch to accountsContainer and wrapped routes inside switch tags

--- src/containers/CitiesContainer.js
+++ src/containers/CitiesContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Route, Routes} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import {fetchCities} from "../actions/fetchCities"
 import Cities from '../components/Cities'
 import CityInput from '../components/CityInput'
@@ -18,11 +18,11 @@ class CitiesContainer extends React.Component {
         return (
             <div>
                 
-                
+                <Switch>
                  <Route path='/cities/new' component={CityInput}/>
                  <Route path='/cities/:id' render={(routerProps) => <City {...routerProps} cities={this.props.cities}/>}/>
                  <Route exact path='/cities' render={(routerProps) => <Cities {...routerProps} cities={this.props.cities}/>}/>
-                
+                </Switch>
                 
 
                 
