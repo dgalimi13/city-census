dcb7ccaa121e2d48c7994a9495ad142651a140e8
danny galimi <dannygalimi13@aim.com>
2022-03-31 11:16:21 -0400

    all three routes functioning properly

--- src/components/City.js
+++ src/components/City.js
@@ -1,15 +1,17 @@
 import React from 'react'
+// import {Redirect} from 'react-router-dom'
 
 
 const City = (props) => {
 
-    console.log(props)
+    
 
     let city = props.cities[props.match.params.id -1]
     console.log(city)
 
     return (
     <li>
+        {/* {city ? null : <Redirect to='/cities'/>} */}
         {city ? city.name : null} - {city ? city.population : null}
     </li>
     )
