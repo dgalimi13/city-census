a54caf5ee0dc29881f686e3ff989653d732c06cc
danny galimi <dannygalimi13@aim.com>
2022-04-01 08:49:53 -0400

    added state variable with empty values for controlled form functionality, added handleChange method to add to event listener in form

--- src/components/RelocationsInput.js
+++ src/components/RelocationsInput.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
-
 class RelocationsInput extends React.Component {
+
+    state = {
+        kind: '',
+        amount: ''
+    }
+
+    handleChange = () => {
+
+    }
 
     render() {
         return (
             <div>
                 <form>
                     <label>Relocation Type:</label>
-                    <select>
+                    <select name="kind" value={this.state.kind} onChange={this.handleChange}>
                         <option>departure</option>
                         <option>arrival</option>
                     </select>
+                    <label>Relocation amount</label>
+                    <input type="text" name="amount" value={this.state.amount}/>
                 </form>
             </div>
         )
